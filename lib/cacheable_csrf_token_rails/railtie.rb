8c6c5ec3617dcf2b3831fadf95395f63beabe608
Matt Button <matthew@sigswitch.com>
2013-08-23 16:00:15 +0100

    Insert middleware after ActionDispatch::ParamsParser
    Other apps that use middleware (e.g. Devise/Warden) use their own
    middleware, and can render pages before lower middlewares can intercept
    the response.

--- lib/cacheable_csrf_token_rails/railtie.rb
+++ lib/cacheable_csrf_token_rails/railtie.rb
@@ -2,7 +2,7 @@
 module CacheableCsrfTokenRails
   class Railtie < Rails::Railtie
     initializer "cacheable_csrf_token_rails.configure_rails_initialization" do |app|
-      app.middleware.use ::CacheableCsrfTokenRails::Middleware
+      app.middleware.insert_after ActionDispatch::ParamsParser, ::CacheableCsrfTokenRails::Middleware
     end
   end
 end
