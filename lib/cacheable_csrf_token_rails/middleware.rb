841780f3b16b995018456059c46f84c9286c8cc8
Matt Button <matthew@sigswitch.com>
2013-10-03 11:39:09 +0100

    Only log if the requested page is an html page, refs #4

--- lib/cacheable_csrf_token_rails/middleware.rb
+++ lib/cacheable_csrf_token_rails/middleware.rb
@@ -7,23 +7,28 @@ module CacheableCsrfTokenRails
     end
 
     def call(env)
-      status, headers, response = @app.call(env)
-
-      [status, headers, replace_token_in_body(response, env)]
+      replace_token(*[env].concat(@app.call(env)))
     end
 
     private
 
-    def replace_token_in_body(response, env)
-      is_mappable = response.respond_to?(:map)
+    def replace_token(env, status, headers, body)
+      is_mappable = body.respond_to?(:map)
       token       = extract_token_from_env(env)
 
       if is_mappable && token
-        response.map { |b| b.gsub(placeholder, token) }
-      else
-        logger.log lib: :cacheable_csrf_token_rails, at: :token_not_replaced, request_uri: env['REQUEST_URI'], response_enumerable: is_mappable, token: token
-        response
+        body.map! { |b| b.gsub(placeholder, token) }
+      elsif String(headers['Content-Type']).include?('text/html')
+        logger.log(
+          lib: :cacheable_csrf_token_rails,
+          at: :token_not_replaced,
+          request_uri: env['REQUEST_URI'],
+          response_enumerable: is_mappable,
+          token: token
+        )
       end
+
+      [status, headers, body]
     end
 
     def placeholder
